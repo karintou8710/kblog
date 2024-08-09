b8d473bf810cf71cfe0d30be543d8041985801de
karintou8710 <karintou8710@gmail.com>
2024-08-09 14:26:47 +0900

    feat: ロゴをリンクに

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="bg-gray-700 text-white h-16 px-4 flex items-center">
-      <h1 className="text-xl font-bold">karintouのブログ</h1>
+      <Link className="text-xl font-bold" href="/">
+        <span>karintouのブログ</span>
+      </Link>
     </header>
   );
 }
