37205b833a0594855915e0c539b52fed0a1c1f52
karintou8710 <karintou8710@gmail.com>
2024-08-09 13:06:01 +0900

    feat: Header追加

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-gray-700 text-white h-16 px-4 flex items-center">
+      <h1 className="text-xl font-bold">karintouのブログ</h1>
+    </header>
+  );
+}
