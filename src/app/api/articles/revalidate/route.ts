a510a726d3eea4a77b4d7d8fe599f771d2cfaa27
karintou8710 <karintou8710@gmail.com>
2024-08-09 16:27:26 +0900

    refactor: APIのバリデーションをZodに

--- src/app/api/articles/revalidate/route.ts
+++ src/app/api/articles/revalidate/route.ts
@@ -1,16 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
 import { revalidateArticle } from "@/server/libs/revalidate";
+import { articleRevalidateSchema } from "@/server/libs/schema";
+import { ZodError } from "zod";
 
 export async function POST(request: NextRequest) {
   try {
-    const { id }: { id?: any } = await request.json();
+    const { id } = articleRevalidateSchema.parse(await request.json());
+    revalidateArticle(id);
 
-    if (typeof id === "string") {
-      revalidateArticle(id);
-    }
     return NextResponse.json({ message: "revalidate articles" });
-  } catch {
-    return NextResponse.json({ message: "incorrect input" });
+  } catch (e) {
+    if (e instanceof ZodError || e instanceof SyntaxError) {
+      return NextResponse.json(
+        { message: "incorrect input" },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    console.error(e);
+
+    return NextResponse.json(
+      { message: "unexpected error" },
+      {
+        status: 500,
+      }
+    );
   }
 }
---
+++ src/server/libs/schema.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+export const articleRevalidateSchema = z.object({
+  id: z.string(),
+});
+
+export type ArticleRevalidateSchema = z.infer<typeof articleRevalidateSchema>;
