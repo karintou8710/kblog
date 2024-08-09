1a50c5ecd1c7e2b812251713a3be67df9bf53412
karintou8710 <karintou8710@gmail.com>
2024-08-09 13:06:15 +0900

    feat: shadcn/ui初期化

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Header from "./_components/header";
+import { cn } from "@/lib/utils";
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Home | Karintou Blog",
@@ -16,7 +19,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable
+        )}
+      >
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
