ea170c87ae6d2647cc15b9c311f082d4a1523d13
karintou8710 <karintou8710@gmail.com>
2024-12-27 19:23:54 +0900

    feat: NotoSansJPを使用

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,9 @@ const config = {
   ],
   prefix: '',
   theme: {
+    fontFamily: {
+      sans: ['var(--font-noto-sans-jp)'],
+    },
     container: {
       center: true,
       padding: '2rem',
