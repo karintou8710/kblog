a5afa64fe93bc1a19211b7998ac7274a8b253f6a
karintou8710 <karintou8710@gmail.com>
2025-05-17 19:08:12 +0900

    feat: aboutにリンク追加

--- src/app/about/_components/user-profile.tsx
+++ src/app/about/_components/user-profile.tsx
@@ -1,6 +1,10 @@
+import { GitHubLogoIcon } from '@radix-ui/react-icons'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import UserIcon from '@/assets/icon.jpg'
+import XIcon from '@/assets/x-logo.svg'
+import { Button } from '@/components/ui/button'
 
 export default function UserProfile() {
   return (
@@ -17,11 +21,26 @@ export default function UserProfile() {
           <div className='mt-4 text-center font-bold'>karintou</div>
           <div className='mx-auto mt-1 h-1 w-16 rounded-full bg-blue-500' />
         </div>
+
         <div className='mt-3'>
           <p className='mt-2'>
             このブログは日々の日常を中心に不定期更新します。食べたご飯で埋め尽くされるかもしれない。たまに技術に関するつぶやきをするかも。
           </p>
           <p className='mt-4'>趣味は模索中。最近は外食とポーカーが好き。</p>
+
+          <div className='mt-7 flex items-center gap-x-3'>
+            <Button variant='icon' size='icon' asChild className='size-6'>
+              <Link href='https://github.com/karintou8710' target='_blank'>
+                <GitHubLogoIcon className='size-6 text-black opacity-40 hover:opacity-100' />
+              </Link>
+            </Button>
+
+            <Button variant='icon' size='icon' asChild className='size-5'>
+              <Link href='https://x.com/karintou74073' target='_blank'>
+                <XIcon className='size-5 text-black opacity-40 hover:opacity-100' />
+              </Link>
+            </Button>
+          </div>
         </div>
       </div>
     </div>
